2b4d38ee16715cdc9f246c5a5ea5e83fc28530ff
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-09-11 08:20:09 -0400

    Initial

--- SIGpi_menus.sh
+++ SIGpi_menus.sh
@@ -14,18 +14,32 @@
 
 # Source Directory
 SIGPI_SOURCE=$HOME/source
-# SIGpi Home directory
-SIGPI_HOME=$HOME/source/SIGbox
-# SIGpi Menuitems directory
-SIGPI_MENUITEMS=$HOME/source/SIGbox/menuitems
 
-DTOP_CATEGORY=SigPi
-DTOP_DIRECTORY=/usr/share/desktop-directories
-DTOP_FILE=/usr/share/applications
-DTOP_ICONS=/usr/share/icons
+# Home directory
+SIGPI_HOME=$HOME/source/SIGbox
+
+# Desktop directories
+SIGPI_DESKTOP=$SIGPI_HOME/desktop
+SIGPI_BACKGROUNDS=$IGPI_DESKTOP/backgrounds
+SIGPI_ICONS=$IGPI_DESKTOP/icons
+SIGPI_LOGO=$IGPI_DESKTOP/logo
+SIGPI_MENU=$IGPI_DESKTOP/menu
+SIGPI_BACKGROUNDS=$IGPI_DESKTOP/backgrounds
+
+# Desktop Destination Directories
+DESKTOP_DIRECTORY=/usr/share/desktop-directories
+DESKTOP_FILES=/usr/share/applications
+DESKTOP_ICONS=/usr/share/icons
 #DTOP_XDG_MENU=/usr/local/share/extra-xdg-menus
 
-sudo cp $SIGPI_MENUITEMS/sigpi_example.desktop $DTOP_FILE
+# SigPi Menu category
+SIGPI_MENU_CATEGORY=SigPi
+
+#
+# Copy Menuitems into relevant directories
+# 
+
+sudo cp $SIGPI_MENUITEMS/sigpi_example.desktop $DESKTOP_FILES
 sudo cp $SIGPI_MENUITEMS/SigPi.directory $DTOP_DIRECTORY
 sudo cp $SIGPI_MENUITEMS/*.png $DTOP_ICONS
 
@@ -33,60 +47,87 @@ sudo cp $SIGPI_MENUITEMS/*.png $DTOP_ICONS
 # Add SigPi Category for each installed application
 #
 
-#sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/audacity.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/CubicSDR.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/flarq.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/fldigi.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/flrig.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/gnuradio-grc.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/gpredict.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/gqrx.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/mumble.desktop
-#sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/pavucontrol.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/qsstv.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/xastir.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" $DTOP_FILE/wsjtx.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" /usr/local/share/applications/direwolf.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" /usr/local/share/Lime/Desktop/lime-suite.desktop
-sudo sed -i "s/Categories.*/Categories=$DTOP_CATEGORY/" /opt/install/sdrangel/share/applications/sdrangel.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/CubicSDR.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/flarq.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/fldigi.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/flrig.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/gnuradio-grc.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/gpredict.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/gqrx.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/mumble.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/qsstv.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/wsjtx.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" $DESKTOP_FILES/xastir.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" /usr/local/share/applications/direwolf.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" /usr/local/share/Lime/Desktop/lime-suite.desktop
+sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY/" /opt/install/sdrangel/share/applications/sdrangel.desktop
 
+##
 ## NOTES:
+##
+: '
 
-#
-# DESKTOP FILES
-#
-# /usr/share/applications/audacity.desktop
-# /usr/share/applications/CubicSDR.desktop
-# /usr/share/applications/flarq.desktop
-# /usr/share/applications/fldigi.desktop
-# /usr/share/applications/flrig.desktop
-# /usr/share/applications/gnuradio-grc.desktop
-# /usr/share/applications/gpredict.desktop
-# /usr/share/applications/gqrx.desktop
-# /usr/share/applications/mumble.desktop
-# /usr/share/applications/pavucontrol.desktop
-# /usr/share/applications/qsstv.desktop
-# /usr/share/applications/xastir.desktop
-# /usr/share/applications/wsjtx.desktop
-# /usr/local/share/applications/direwolf.desktop
-# /usr/local/share/Lime/Desktop/lime-suite.desktop
-# /opt/install/sdrangel/share/applications/sdrangel.desktop
+ The following are installed with package installs
 
-#
-# ICONS
-#
-# /usr/share/pixmaps/CQ.png   (Ham Radio menu icon)
-# /usr/share/pixmaps/direwolf_icon.png
-# /usr/share/icons/hicolor/64x64/apps/fldigi.png
-# /usr/share/icons/hicolor/48x48/apps/flarq.png
-# /usr/share/qsstv/qsstv.png
-# 
+ CubicSDR.desktop
+ flarq.desktop
+ fldigi.desktop
+ flrig.desktop
+ gnuradio-grc.desktop
+ gpredict.desktop
+ gqrx.desktop
+ mumble.desktop
+ qsstv.desktop
+ wsjtx.desktop
+ xastir.desktop
 
-#
-# DESKTOP-DIRECTORIES
-#
-# /usr/share/desktop-directories/HamRadio.directory
+ Need to create desktop files for
 
+ AX.25 and utilities
+ axlisten (which needs direwolf running first)
+ axcall (which needs direwolf running first)
+ Murmur Server
+ LimeSDR
+ GPSd and Chrony
+ Kismet
+ SDRangel
+ VOX for SDRangel
+ Gpredict
+ DireWolf 1.7
+ Linpac
+
+
+ DESKTOP FILES
+
+ /usr/share/applications/audacity.desktop
+ /usr/share/applications/CubicSDR.desktop
+ /usr/share/applications/flarq.desktop
+ /usr/share/applications/fldigi.desktop
+ /usr/share/applications/flrig.desktop
+ /usr/share/applications/gnuradio-grc.desktop
+ /usr/share/applications/gpredict.desktop
+ /usr/share/applications/gqrx.desktop
+ /usr/share/applications/mumble.desktop
+ /usr/share/applications/pavucontrol.desktop
+ /usr/share/applications/qsstv.desktop
+ /usr/share/applications/xastir.desktop
+ /usr/share/applications/wsjtx.desktop
+ /usr/local/share/applications/direwolf.desktop
+ /usr/local/share/Lime/Desktop/lime-suite.desktop
+ /opt/install/sdrangel/share/applications/sdrangel.desktop
+
+ ICONS
+
+ /usr/share/pixmaps/CQ.png   (Ham Radio menu icon)
+ /usr/share/pixmaps/direwolf_icon.png
+ /usr/share/icons/hicolor/64x64/apps/fldigi.png
+ /usr/share/icons/hicolor/48x48/apps/flarq.png
+ /usr/share/qsstv/qsstv.png
+ 
+ DESKTOP-DIRECTORIES
+
+ /usr/share/desktop-directories/HamRadio.directory
+'
 
 
 
