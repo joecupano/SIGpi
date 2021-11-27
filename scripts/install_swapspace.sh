2bfb7c7d4b5ffb5b9cd78b56c18e2f7c94fb74ee
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-11-27 04:02:58 -0500

    MOve swap to script for SIGprojects template

---
+++ scripts/install_swapspace.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+###
+### SIGpi
+###
+### installer_swapspace
+###
+
+# Artemis
+echo -e "${SIGPI_BANNER_COLOR}"
+echo -e "${SIGPI_BANNER_COLOR} ##"
+echo -e "${SIGPI_BANNER_COLOR} ##   Swapspace"
+echo -e "${SIGPI_BANNER_COLOR} ##"
+echo -e "${SIGPI_BANNER_RESET}"
+
+if [ -f /swapfile ]; then
+    echo "Removing existing swapfile"
+    swapoff /swapfile
+    sleep 5
+    sudo rm -rf /swapfile
+fi
+
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+
+echo -e "${SIGPI_BANNER_COLOR}"
+echo -e "${SIGPI_BANNER_COLOR} ##   Swapspace Complete"
+echo -e "${SIGPI_BANNER_RESET}"
