e9f9d0391a9ee2d6cb5a4ff343244384f4cb407d
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-11-27 13:49:54 -0500

    Make install output more verbose

--- scripts/install_fftw-wisdom.sh
+++ scripts/install_fftw-wisdom.sh
@@ -20,7 +20,7 @@ mkdir f4exb
 cd f4exb
 # Generate a new wisdom file for FFT sizes : 128, 256, 512, 1024, 2048, 4096, 8192, 16384 and 32768.
 # This will take a very long time.
-fftwf-wisdom -n -o fftw-wisdom 128 256 512 1024 2048 4096 8192 16384 32768
+fftwf-wisdom -v -n -o fftw-wisdom 128 256 512 1024 2048 4096 8192 16384 32768
 
 
 echo -e "${SIGPI_BANNER_COLOR}"
