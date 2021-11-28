eab9b409581eddf9f4779edbc81d803827eb1af0
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-11-27 21:39:05 -0500

    cleanup

---
+++ scripts/_install_template.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+###
+### SIGpi
+###
+### _install_template
+###
+
+echo -e "${SIGPI_BANNER_COLOR}"
+echo -e "${SIGPI_BANNER_COLOR} ##"
+echo -e "${SIGPI_BANNER_COLOR} ##   Install <PACKAGE>"
+echo -e "${SIGPI_BANNER_COLOR} ##"
+echo -e "${SIGPI_BANNER_RESET}"
+
+# DEPENDENCIES
+
+# INSTALL
+
+
+echo -e "${SIGPI_BANNER_COLOR}"
+echo -e "${SIGPI_BANNER_COLOR} ##   <PACKAGE> Installed"
+echo -e "${SIGPI_BANNER_RESET}"
