3ef88551ada7fb8704d652533eb4b9e6308914ae
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-12-08 16:01:21 -0500

    update to package model

--- scripts/install_rtltcp-srv.sh
+++ scripts/install_rtltcp-srv.sh
@@ -3,9 +3,52 @@
 ###
 ### SIGPI
 ###
-### install_rtltcp-srv
+### package_rtltcp-srv
 ###
 
+###
+### 20211208-1200  Currently default to install to keep script backward compatible
+###
+
+# REMOVE
+if ( $1 == "remove"); then
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_COLOR} ##   Remove RTL_TCP Service"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_RESET}"
+
+    sudo systemctl stop rtlsdr.service
+    sleep 5
+    sudo systemctl disable rtlsdr.service
+    sudo rm -rf /etc/systemd/system/rtlsdr.service
+
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##   RTL_TCP Service Removed"
+    echo -e "${SIGPI_BANNER_RESET}"
+fi
+
+# PURGE
+if ( $1 == "purge"); then
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_COLOR} ##   Purge RTL_TCP Service"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_RESET}"
+
+    sudo systemctl stop rtlsdr.service
+    sleep 5
+    sudo systemctl disable rtlsdr.service
+    sudo rm -rf /etc/systemd/system/rtlsdr.service
+
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##   RTL_TCP Service Purged"
+    echo -e "${SIGPI_BANNER_RESET}"
+fi
+
+# DEPENDENCIES
+
+# INSTALL
 echo -e "${SIGPI_BANNER_COLOR}"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_COLOR} ##   Install RTL_TCP Service"
--- scripts/install_soapysdr-srv.sh
+++ scripts/install_soapysdr-srv.sh
@@ -3,9 +3,52 @@
 ###
 ### SIGPI
 ###
-### install_soapysdr_srv
+### package_soapysdr_srv
 ###
 
+###
+### 20211208-1200  Currently default to install to keep script backward compatible
+###
+
+# REMOVE
+if ( $1 == "remove"); then
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_COLOR} ##   Remove SoapySDR Service"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_RESET}"
+
+    sudo systemctl stop soapysdr.service
+    sleep 5
+    sudo systemctl disable soapysdr.service
+    sudo rm -rf /etc/systemd/system/soapysdr.service
+
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##   SoapySDR Service Removed"
+    echo -e "${SIGPI_BANNER_RESET}"
+fi
+
+# PURGE
+if ( $1 == "purge"); then
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_COLOR} ##   Purge SoapySDR Service"
+    echo -e "${SIGPI_BANNER_COLOR} ##"
+    echo -e "${SIGPI_BANNER_RESET}"
+
+    sudo systemctl stop soapysdr.service
+    sleep 5
+    sudo systemctl disable soapysdr.service
+    sudo rm -rf /etc/systemd/system/soapysdr.service
+
+    echo -e "${SIGPI_BANNER_COLOR}"
+    echo -e "${SIGPI_BANNER_COLOR} ##   SoapySDR Service Purged"
+    echo -e "${SIGPI_BANNER_RESET}"
+fi
+
+# DEPENDENCIES
+
+# INSTALL
 echo -e "${SIGPI_BANNER_COLOR}"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_COLOR} ##   Install SoapySDR Service"
