0c1083aaf470935e322249b2449319d75f531f10
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-12-08 15:37:56 -0500

    Switch to BETA for support of arm64

--- scripts/install_kismet.sh
+++ scripts/install_kismet.sh
@@ -51,7 +51,7 @@ sudo apt-get install -y python3-serial python3-usb python3-dev python3-websocket
 
 cd $SIGPI_SOURCE
 wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key | sudo apt-key add -
-echo 'deb https://www.kismetwireless.net/repos/apt/release/buster buster main' | sudo tee /etc/apt/sources.list.d/kismet.list
+echo 'deb https://www.kismetwireless.net/repos/apt/release/kali kali main' | sudo tee /etc/apt/sources.list.d/kismet.list
 sudo apt update
 sudo apt-get install -y kismet
 #
