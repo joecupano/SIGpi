cad7ccc5ce6d0fe090273a89953ed3660b07222f
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-12-08 16:01:03 -0500

    add comments

--- scripts/install_fftw-wisdom.sh
+++ scripts/install_fftw-wisdom.sh
@@ -3,8 +3,12 @@
 ###
 ### SIGpi
 ###
-### install_sdrangel
+### install_fftw-wisdom
 ###
+
+# REMOVE
+
+# PURGE
 
 # DEPENDENCIES
 
@@ -21,7 +25,6 @@ cd f4exb
 # Generate a new wisdom file for FFT sizes : 128, 256, 512, 1024, 2048, 4096, 8192, 16384 and 32768.
 # This will take a very long time.
 fftwf-wisdom -v -n -o fftw-wisdom 128 256 512 1024 2048 4096 8192 16384 32768
-
 
 echo -e "${SIGPI_BANNER_COLOR}"
 echo -e "${SIGPI_BANNER_COLOR} ##   FFTW-Wisdom Installed"
