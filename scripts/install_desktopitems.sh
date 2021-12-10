ca3c0c2e30c6cbc9ea22588e4c05f89e3f9570e7
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-12-09 21:59:31 -0500

    remove broken unused

--- scripts/install_desktopitems.sh
+++ scripts/install_desktopitems.sh
@@ -39,15 +39,8 @@ fi
 
 # Add Desktop links
 sudo cp $SIGPI_SOURCE/desktop/artemis.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/flrig-1.4.2/data/flrig.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/fldigi-4.1.20/data/flarq.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/fldigi-4.1.20/data/fldigi.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/gnuradio/grc/scripts/freedesktop/gnuradio-grc.desktop $DESKTOP_FILES
 sudo cp $SIGPI_SOURCE/LimeSuite/Desktop/lime-suite.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/desktop/sdrangel.desktop $DESKTOP_FILES
 sudo cp $SIGPI_SOURCE/SDRPlusPlus/build/sdrpp.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/qsstv/qsstv.desktop $DESKTOP_FILES
-sudo cp $SIGPI_SOURCE/desktop/xastir.desktop $DESKTOP_FILES
 sudo cp $SIGPI_DESKTOP/SigPi.directory $DESKTOP_DIRECTORY
 sudo cp $SIGPI_DESKTOP/SigPi.menu $DESKTOP_XDG_MENU
 sudo cp $SIGPI_DESKTOP/sigpi_home.desktop $HOME/Desktop/SIGpi.desktop
