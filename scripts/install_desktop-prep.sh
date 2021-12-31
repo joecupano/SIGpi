6ec85b75b0c18b856b54a6bdafab119233bf7060
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-12-31 10:48:26 -0500

    desktop updates

--- scripts/install_desktop-prep.sh
+++ scripts/install_desktop-prep.sh
@@ -19,7 +19,7 @@ echo -e "${SIGPI_BANNER_RESET}"
 #/.local/share/desktop-directories
 
 # Copy SIGpi commands into /usr/local/bin
-sudo cp $SIGPI_HOME/scripts/SIGpi_exec-in-shell /usr/local/bin/SIGpi_exec-in-shell 
+sudo cp $SIGPI_HOME/scripts/SIGpi_exec-in-shell.sh /usr/local/bin/SIGpi_exec-in-shell 
 sudo cp $SIGPI_HOME/scripts/SIGpi_pusher.sh /usr/local/bin/SIGpi_pusher
 sudo cp $SIGPI_HOME/scripts/SIGpi_popper.sh /usr/local/bin/SIGpi_popper
 sudo cp $SIGPI_HOME/scripts/SIGpi.sh /usr/local/bin/SIGpi
