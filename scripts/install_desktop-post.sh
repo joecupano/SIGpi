6345be15d3527dc642c0d5f27d0b8dfb215ac87d
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2023-01-31 01:32:26 -0500

    Desktop file setup fix

--- scripts/install_desktop-post.sh
+++ scripts/install_desktop-post.sh
@@ -12,49 +12,6 @@ echo -e "${SIGPI_BANNER_COLOR} ##   Desktop Post"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_RESET}"
 
-# XDG-USER-DIR
-#/.local
-#/.local/share
-#/.local/share/applications
-#/.local/share/desktop-directories
-
-# Copy SIGpi Desktop files to XDG Dirs
-#sudo cp $SIGPI_DESKTOP/*.desktop $DESKTOP_FILES
-sudo cp $SIGPI_ICONS/* $DESKTOP_ICONS
-#sudo cp $SIGPI_DESKTOP/SIGpi.directory $DESKTOP_DIRECTORY
-#sudo cp $SIGPI_DESKTOP/SIGpi.menu $DESKTOP_XDG_MENU
-sudo cp $SIGPI_DESKTOP/sigpi_home.desktop $HOME/Desktop/SIGpi.desktop
-sudo chmod 755 $HOME/Desktop/SIGpi.desktop
-sudo chown $USER $HOME/Desktop/SIGpi.desktop
-sudo cp $SIGPI_DESKTOP/sigidwiki.desktop $HOME/Desktop/sigidwiki.desktop
-sudo chmod 755 $HOME/Desktop/sigidwiki.desktop
-sudo chown $USER $HOME/Desktop/sigidwiki.desktop
-
-# Add SIGpi Category for installed applications
-#sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/artemis.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/CubicSDR.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/gnuradio-grc.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/gpredict.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/gqrx-sdr.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/lime-suite.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/sdrangel.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/sdrpp.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/wireshark.desktop
-sudo sed -i "s/Categories.*/Categories=$SIGPI_MENU_CATEGORY;/" $DESKTOP_FILES/xastir.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/direwolf.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/flarq.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/fldigi.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/flrig.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/linpac.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/qsstv.desktop
-#sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/wsjtx.desktop
-#sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/message_aggregator.desktop
-sudo sed -i "s/Categories.*/Categories=$HAMRADIO_MENU_CATEGORY;/" $DESKTOP_FILES/xastir.desktop
-
-# Add installed applications into SIGpi menu
-xdg-desktop-menu install --novendor --noupdate $DESKTOP_DIRECTORY/SIGpi.directory $DESKTOP_FILES/sigidwiki.desktop
-xdg-desktop-menu install --novendor --noupdate $DESKTOP_DIRECTORY/SIGpi.directory $DESKTOP_FILES/sigpi_home.desktop
-
 if [[ "$SIGPI_HWARCH" == "x86_64" ]]; then
     sudo cp $SIGPI_HOME/backgrounds/* /usr/share/backgrounds
     gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop',\
--- scripts/install_desktop-prep.sh
+++ scripts/install_desktop-prep.sh
@@ -12,7 +12,7 @@ echo -e "${SIGPI_BANNER_COLOR} ##   Desktop Prep"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_RESET}"
 
-# Local Settings Extras
+# Local Settings Extras XDG-USER-DIR
 #/.local
 #/.local/share
 #/.local/share/applications
@@ -22,8 +22,10 @@ echo -e "${SIGPI_BANNER_RESET}"
 sudo cp $SIGPI_HOME/scripts/SIGpi_exec-in-shell.sh /usr/local/bin/SIGpi_exec-in-shell 
 sudo cp $SIGPI_HOME/scripts/SIGpi.sh /usr/local/bin/SIGpi
 
+# Copy icons
+sudo cp $SIGPI_ICONS/* $DESKTOP_ICONS
+
 # Copy Background images
-## What operating system are we?
 if [[ "$SIGPI_HWARCH" == "x86_64" ]]; then
     sudo cp $SIGPI_HOME/backgrounds/* /usr/share/backgrounds
     # Change Background image
@@ -36,11 +38,25 @@ if [[ "$SIGPI_HWARCH" == "aarch64" ]]; then
     pcmanfm --set-wallpaper=/usr/share/rpd-wallpaper/SIGpi_wallpaper.png
 fi
 
-# Copy SIGpi Desktop Setup files to XDG Dirs
+# Copy SIGpi Menu and Directory files to XDG Dirs
 sudo cp $SIGPI_DESKTOP/SIGpi.directory $DESKTOP_DIRECTORY
 sudo cp $SIGPI_DESKTOP/SIGpishell.directory $DESKTOP_DIRECTORY
 sudo cp $SIGPI_DESKTOP/SIGpi.menu $DESKTOP_XDG_MENU
 sudo cp $SIGPI_DESKTOP/SIGpishell.menu $DESKTOP_XDG_MENU
+
+
+# Copy SIGpi Desktop files to XDG Dirs
+#sudo cp $SIGPI_DESKTOP/*.desktop $DESKTOP_FILES
+sudo cp $SIGPI_DESKTOP/sigpi_home.desktop $HOME/Desktop/SIGpi.desktop
+sudo chmod 755 $HOME/Desktop/SIGpi.desktop
+sudo chown $USER $HOME/Desktop/SIGpi.desktop
+sudo cp $SIGPI_DESKTOP/sigidwiki.desktop $HOME/Desktop/sigidwiki.desktop
+sudo chmod 755 $HOME/Desktop/sigidwiki.desktop
+sudo chown $USER $HOME/Desktop/sigidwiki.desktop
+
+# Add installed applications into SIGpi menu
+xdg-desktop-menu install --novendor --noupdate $DESKTOP_DIRECTORY/SIGpi.directory $DESKTOP_FILES/sigidwiki.desktop
+xdg-desktop-menu install --novendor --noupdate $DESKTOP_DIRECTORY/SIGpi.directory $DESKTOP_FILES/sigpi_home.desktop
 
 echo -e "${SIGPI_BANNER_COLOR}"
 echo -e "${SIGPI_BANNER_COLOR} ##   Desktop Prep Complete"
