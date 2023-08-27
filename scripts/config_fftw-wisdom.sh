d0a15b108f408e9defa696e906276e43c373a6a0
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2023-08-27 03:52:52 -0400

    FFT-Wisdom repackaging and finel generation

--- scripts/config_fftw-wisdom.sh
+++ scripts/config_fftw-wisdom.sh
@@ -15,17 +15,16 @@
 # INSTALL
 echo -e "${SIGPI_BANNER_COLOR}"
 echo -e "${SIGPI_BANNER_COLOR} ##"
-echo -e "${SIGPI_BANNER_COLOR} ##   FFTW-Wisdom file (ETA: +10 Minutes)"
+echo -e "${SIGPI_BANNER_COLOR} ##   Generate FFTW-Wisdom file (ETA: +10 Minutes)"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_RESET}"
 
-cd $HOME/.config/
-mkdir f4exb
-cd f4exb
+
+cd $SIGPI_ETC
 # Generate a new wisdom file for FFT sizes : 128, 256, 512, 1024, 2048, 4096, 8192, 16384 and 32768.
 # This will take a very long time.
 fftwf-wisdom -v -n -o fftw-wisdom 128 256 512 1024 2048 4096 8192 16384 32768
 
 echo -e "${SIGPI_BANNER_COLOR}"
-echo -e "${SIGPI_BANNER_COLOR} ##   FFTW-Wisdom Installed"
+echo -e "${SIGPI_BANNER_COLOR} ##   FFTW-Wisdom Generated in $SIGPI_ETC"
 echo -e "${SIGPI_BANNER_RESET}"
