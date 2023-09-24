cea2bfbe32cd0230990dae6a75fd5f51addf0533
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2023-09-23 23:35:08 -0400

    Change desktop checks

--- scripts/install_desktop-post.sh
+++ scripts/install_desktop-post.sh
@@ -12,17 +12,15 @@ echo -e "${SIGPI_BANNER_COLOR} ##   Desktop Post"
 echo -e "${SIGPI_BANNER_COLOR} ##"
 echo -e "${SIGPI_BANNER_RESET}"
 
-if [[ "$SIGPI_HWARCH" == "x86_64" ]]; then
+if [[ "$SIGPI_OSNAME" == "Debian GNU/Linux 11 (bullseye)" ]]; then
+    sudo cp $SIGPI_HOME/backgrounds/* /usr/share/rpd-wallpaper
+    # Change Background image
+    pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/SIGpi_wallpaper.png
+else
     sudo cp $SIGPI_HOME/backgrounds/* /usr/share/backgrounds
     gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop',\
      'sdrangel.desktop', 'sdrpp.desktop', 'gnuradio-grc.desktop',\
      'rtl_433.desktop', 'sigidwiki.desktop', 'sigpi_home.desktop']"
-fi
-
-if [[ "$SIGPI_HWARCH" == "aarch64" ]]; then
-    sudo cp $SIGPI_HOME/backgrounds/* /usr/share/rpd-wallpaper
-    # Change Background image
-    pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/SIGpi_wallpaper.png
 fi
 
 echo -e "${SIGPI_BANNER_COLOR}"
